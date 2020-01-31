a45799aaaa3d0ff630b24e825ae1cc7c2b2eee7e
MAKY0044 <makyjustiniano4444@gmail.com>
2020-01-31 12:32:27 -0400

    Agregamos la base de datos y agregamos un contralor para agregar productos

---
+++ Shop2.Web/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+namespace Shop2.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Shop2.Web.Data.Entities;
+   
+    public class DataContext:DbContext
+    {
+       public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
